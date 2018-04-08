853a47ac753df4306dbba7cfb5fc7b3dfd6ffc2c
JonathanLayman <lndbearshark@gmail.com>
2018-04-08 09:19:43 -0600

    Tile-based game Part 1: Setting up https://youtu.be/3UxnelT9aCo

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Tile Based Game
 import pygame as pg
+import sys
 import random
 from settings import *
+from sprites import *
 
 
 class Game:
@@ -10,8 +12,10 @@ class Game:
         pg.init()
         pg.mixer.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption("My Game")
+        pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
+        self.dt = None
+        pg.key.set_repeat(500, 100)
         self.running = True
 
         # Running bool
@@ -19,38 +23,71 @@ class Game:
 
         # Initialize sprite groups
         self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+
+        # Set up Sprite Variables
+        self.player = None
+
+
+        # Load Data
+        self.load_data()
+
+    def load_data(self):
+        pass
 
     def new(self):
         # Start a new game
-        self.run()
+        self.player = Player(self,0, 3)
+        for x in range(10, 20):
+            Wall(self, x, 5)
 
     def run(self):
         # Game loop
         while self.playing:
-            self.clock.tick(FPS)
+            self.dt = self.clock.tick(FPS) / 1000
             self.events()
             self.update()
             self.draw()
 
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
     def update(self):
         # Game Loop - Update
         self.all_sprites.update()
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        # Game Loop - Draw
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        # *after* drawing everything, flip the display
+        pg.display.flip()
 
     def events(self):
         # Game Loop - Events
         for event in pg.event.get():
             # check for closing window
             if event.type == pg.QUIT:
-                if self.playing:
-                    self.playing = False
-                    self.running = False
-
-    def draw(self):
-        # Game Loop - Draw
-        self.screen.fill(BLACK)
-        self.all_sprites.draw(self.screen)
-        # *after* drawing everything, flip the display
-        pg.display.flip()
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
 
     def show_start_screen(self):
         # game splash/start screen
@@ -65,6 +102,5 @@ g = Game()
 g.show_start_screen()
 while g.running:
     g.new()
+    g.run()
     g.show_go_screen()
-
-pg.quit()
--- settings.py
+++ settings.py
@@ -1,12 +1,20 @@
-# Game options / settings
-TITLE = "MY GAME"
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-
 # define colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+YELLOW = (255, 255, 0)
+
+# Game options / settings
+TITLE = "Tilemap Demo"
+WIDTH = 1024
+HEIGHT = 768
+FPS = 60
+BGCOLOR = DARKGREY
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
